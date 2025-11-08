7780648a3b65e3b4384cd71b3d9450127cd74272
Nicolasmelo <nicomelo895@gmail.com>
2025-11-07 22:01:39 -0500

    Cambio en el saludo - De boton a saludo automatico al ingresar nombre

--- src/app/saludo/saludo.component.ts
+++ src/app/saludo/saludo.component.ts
@@ -1,20 +1,17 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { CapitalizarPipe } from '../capitalizar.pipe';
 
 @Component({
   selector: 'app-saludo',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, CapitalizarPipe],
   templateUrl: './saludo.component.html',
   styleUrls: ['./saludo.component.scss'],
 })
 
 export class SaludoComponent {
   nombre: string = '';
-  mensaje: string = '';
-
-  saludar() {
-    this.mensaje = this.nombre ? `Hola ${this.nombre}` : 'Por favor escribe tu nombre';
-  }
+  // Ahora el saludo se muestra en tiempo real en la plantilla
 }
